df64a3df9217c65d22a4b52682f8f28bcd1a6e26
Karina Cristo <karinacristonegocios@gmail.com>
2022-03-18 12:24:57 -0300

    instalei o nodemon e criei a rota post

--- src/controller/agendamentosController.js
+++ src/controller/agendamentosController.js
@@ -1,6 +1,15 @@
 const agendamentosController = app => {
+
     app.get('/agendamentos', (req, res) => {
-        res.send('rodando GET na rota atendimentos')
+        res.json({
+            "nome" : "karina"
+        })
+    })
+
+    app.post('/agendamentos', (req, res) => {
+        res.json({
+            "mensagem" : "seu agendamento foi salvo com sucesso!"
+        })
     })
 }
 
