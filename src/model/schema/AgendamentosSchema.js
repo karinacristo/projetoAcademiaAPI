a832d228fbf12bfe01d316b2a125feac157c71dd
Karina Cristo <karinacristonegocios@gmail.com>
2022-03-25 21:55:18 -0300

    finalizando

--- src/model/schema/AgendamentosSchema.js
+++ src/model/schema/AgendamentosSchema.js
@@ -1,18 +1,12 @@
 class AgendamentosSchema {
     constructor(cliente, data, profissional, período, serviço, observações){
-        this.cliente = this._validaCliente(cliente)
+        this.cliente = cliente
         this.data = data
         this.profissional = profissional
         this.período = período
         this.serviço = serviço
         this.observações = observações
     }
-    _validaCliente = (cliente)=>{
-        if (cliente == null || cliente.length == 0) {
-          throw new Error('Digite um nome válido')
-        }
-        return cliente
-     }
 }
 
 export default AgendamentosSchema
